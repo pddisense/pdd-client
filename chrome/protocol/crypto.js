b0dd8ddeefcdef5c385a3db6109ba35710c3427c
Vincent Primault <v.primault@ucl.ac.uk>
2018-12-03 16:43:57 +0000

    Add comment about some ambiguity

--- chrome/protocol/crypto.js
+++ chrome/protocol/crypto.js
@@ -79,6 +79,11 @@ function generateBlindingFactors(publicKeys, key, clientIndex, L, round) {
     publicKeys.forEach((userKey, idx) => {
       const pubKey = importPublicKey(userKey);
       const share = key.derive(pubKey);
+      // Although this is not obvious by just looking at the line,
+      // `share + l + round` is correct because it is equivalent to
+      // `share.toString() + l + round` (meaning that + actually
+      // concatenates strings, as required by the protocol, and not
+      // numbers, as it might be dubious first).
       const n = new BN(hash(share + l + round));
       if (idx < clientIndex) {
         K_il = K_il.add(n);
